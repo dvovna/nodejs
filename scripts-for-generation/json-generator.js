f8b26e1902d16d98bbc069bc0a7978dad7477d3a
dvovna <d.vovna.dv@gmail.com>
2013-08-29 17:38:38 +0300

    generator's implementation;

--- scripts-for-generation/json-generator.js
+++ scripts-for-generation/json-generator.js
@@ -1,6 +1,51 @@
 exports.generate = generate;
 
+var projects = ["World of Tanks", "Umbrella", "Red Alert", "Gnom", "Placer", "UI", "Java"];
+var compainies = ["new arms", "new boobs", "new butt", "new backbone", "new lazer", "patch 100500"];
+var activities = ["link on bla.ru", "banner on tut.by", "banner on google.com", "icon on efinancialcareers.com", "", ""];
+
+var areas = ["header on main form", "footer on main form", "left side", "right side"];
+var partners = ["hubabuba.org", "orbit.by", "google.com", "nodejs.org"];
+
+var refs = [""];
+
+
 //should return correct json
 function generate() {
-	return new Date();
+	return getRandomProjectName();
+}
+
+function getProjects {
+	var projects [],
+		massSize= rand(100);
+
+	for(var i = 0, massSize, i++) {
+		projects.push(getRandomProject());
+	}
+
+	return projects;
+}
+
+function getRandomProject() {
+
+}
+
+function getRandomProjectName() {
+	return projects[Math.floor(rand() * projects.length)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function rand(length) {
+	var len = length || '';
+	return Math.random(len);
 }
